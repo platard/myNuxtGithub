9f62abd4083efbb5925dc2a1731bd461af5f8b45
platara <plat0033@algonquinlive.com>
2023-07-17 20:54:54 -0400

    first commit

---
+++ _nuxt/about.1603d730.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as c}from"./entry.b155952f.js";const o={};function r(a,n){return c(),t("h2",null,"Here my about virtual page")}const _=e(o,[["render",r]]);export{_ as default};
---
+++ _nuxt/index.cd4215d4.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as n}from"./entry.b155952f.js";const r={};function t(o,a){return n(),c("h2",null,"Here my index virtual page")}const _=e(r,[["render",t]]);export{_ as default};
